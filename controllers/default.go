daa81767bf18fbe1ba2ac6b4d0b0c89d85330a77
tonypublic <tonypublic@163.com>
2015-10-13 17:04:00 +0800

    20151013 routine work  Changes to be committed: 	modified:   conf/app.conf 	modified:   controllers/default.go 	new file:   db/OnLineActive/L00001/[Originals]/item.jpg 	new file:   db/OnLineActive/L00001/item.jpg 	new file:   "db/OnLineActive/L00001/\346\212\225\350\236\215\350\265\204\344\277\261\344\271\220\351\203\250\347\272\277\344\270\212\346\264\273\345\212\250\347\254\2541\346\234\237.jpg" 	modified:   static/css/my-app.css 	modified:   static/js/my-app.js 	renamed:    views/about.html -> views/details.html 	modified:   views/index.html

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,25 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	// c.Data["Website"] = "beego.me"
-	// c.Data["Email"] = "astaxie@gmail.com"
+
+	records := new([10]recordBasicInfo)
+	for i, _ := range records {
+		records[i].Title = "走进股市"
+		records[i].Date = "2015.4.14"
+		records[i].Category = "微信 线上活动"
+		records[i].Summary = "大风起兮云飞扬，热点轮换怎么玩，业内人士带你走进A股H股B股。"
+		records[i].ItempicPath = "db/onlineactive/l00001/item.jpg"
+	}
+
+	c.Data["records"] = records
+
 	c.TplNames = "index.html"
+}
+
+type recordBasicInfo struct {
+	Title       string
+	Date        string
+	Category    string
+	Summary     string
+	ItempicPath string
 }
--- static/js/my-app.js
+++ static/js/my-app.js
@@ -11,17 +11,18 @@ var mainView = myApp.addView('.view-main', {
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
+myApp.onPageInit('about', function(page) {
     // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
+    $$('.create-page').on('click', function() {
         createContentPage();
     });
 });
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
+
 function createContentPage() {
-	mainView.router.loadContent(
+    mainView.router.loadContent(
         '<!-- Top Navbar-->' +
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
@@ -44,5 +45,49 @@ function createContentPage() {
         '  </div>' +
         '</div>'
     );
-	return;
+    return;
 }
+
+// 加载flag
+var loading = false;
+
+// 上次加载的序号
+var lastIndex = $$('.list-block li').length;
+
+// 最多可加载的条目
+var maxItems = 60;
+
+// 每次加载添加多少条目
+var itemsPerLoad = 20;
+
+// 注册'infinite'事件处理函数
+$$('.infinite-scroll').on('infinite', function() {
+    // 如果正在加载，则退出
+    if (loading) return;
+    // 设置flag
+    loading = true;
+    // 模拟1s的加载过程
+    setTimeout(function() {
+        // 重置加载flag
+        loading = false;
+        if (lastIndex >= maxItems) {
+            // 加载完毕，则注销无限加载事件，以防不必要的加载
+            myApp.detachInfiniteScroll($$('.infinite-scroll'));
+            // 删除加载提示符
+            $$('.infinite-scroll-preloader').remove();
+            return;
+        }
+
+        // 生成新条目的HTML
+        var html = '';
+        for (var i = lastIndex + 1; i <= lastIndex + itemsPerLoad; i++) {
+            html += '<li class="item-content"><div class="item-inner"><div class="item-title">Item ' + i + '</div></div></li>';
+        }
+
+        // 添加新条目
+        $$('.list-block ul').append(html);
+
+        // 更新最后加载的序号
+        lastIndex = $$('.list-block li').length;
+    }, 1000);
+});
