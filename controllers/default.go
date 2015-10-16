1178aa1c08af50ca1dd3931ed6d5ee8294c38676
tonypublic <tonypublic@163.com>
2015-10-16 17:02:22 +0800

    20151016 routine work  Changes to be committed: 	modified:   controllers/default.go 	new file:   db/OnLineActive/L00001/001.jpg 	new file:   db/OnLineActive/L00001/head.jpg 	renamed:    db/OnLineActive/L00001/item.jpg -> db/OnLineActive/L00001/thumbnails.jpg 	modified:   lastupdate.tmp 	new file:   models/models.go 	modified:   static/css/my-app.css 	modified:   views/details.html 	deleted:    views/form.html 	modified:   views/index.html 	deleted:    views/services.html

--- controllers/default.go
+++ controllers/default.go
@@ -6,12 +6,26 @@ import (
 	"strconv"
 )
 
+//活动列表
 type recordBasicInfo struct {
 	Title       string
 	Date        string
 	Category    string
 	Summary     string
 	ItempicPath string
+}
+
+//活动详细内容
+type onLineActiveDetails struct {
+	RecordNo string
+	Category string
+	Title    string
+	Date     string
+	Form     string
+	Host     string
+	Guest    string
+	Summary  string
+	Content  string
 }
 
 type MainController struct {
@@ -24,9 +38,9 @@ func (this *MainController) Get() {
 	for i, _ := range records {
 		records[i].Title = "走进股市"
 		records[i].Date = "2015.4.14"
-		records[i].Category = "微信 线上活动"
+		records[i].Category = "微信 | 线上活动"
 		records[i].Summary = "大风起兮云飞扬，热点轮换怎么玩，业内人士带你走进A股H股B股。"
-		records[i].ItempicPath = "db/onlineactive/l00001/item.jpg"
+		records[i].ItempicPath = "db/onlineactive/l00001/thumbnails.jpg"
 	}
 
 	this.Data["records"] = records
@@ -52,9 +66,9 @@ func (this *CMSController) ListAddedRecords() {
 	for i, _ := range records {
 		records[i].Title = "走进股市" + strconv.Itoa(i+lastindex)
 		records[i].Date = "2015.4.14"
-		records[i].Category = "微信 线上活动"
+		records[i].Category = "微信 | 线上活动"
 		records[i].Summary = "大风起兮云飞扬，热点轮换怎么玩，业内人士带你走进A股H股B股。"
-		records[i].ItempicPath = "db/onlineactive/l00001/item.jpg"
+		records[i].ItempicPath = "db/onlineactive/l00001/thumbnails.jpg"
 	}
 
 	this.Data["json"] = &records
@@ -66,10 +80,28 @@ func (this *CMSController) ViewDetails() {
 	recordno, _ := this.GetInt(":recordno")
 	fmt.Println(recordno)
 
-	this.Data["content"] = "Here is About page!</p>" +
+	category := "线上活动"
+	title := "走进股市"
+	date := "2015.10.16"
+	form := "微信"
+	host := "张三，李四"
+	guest := "张龙，赵虎"
+	summary := "大风起兮云飞扬，热点轮换怎么玩，业内人士带你走进A股H股B股。"
+	// picPath := "db/onlineactive/l00001/"
+
+	content := "<p>Here is About page!</p>" +
 		"<p>Go back or click  create-page to create dynamic page.</p>" +
 		"<p>Mauris posuere sit amet metus id venenatis. Ut ante dolor, tempor nec commodo rutrum, varius at sem. Nullam ac nisi non neque ornare pretium. Nulla mauris mauris, consequat et elementum sit amet, egestas sed orci. In hac habitasse platea dictumst.</p>" +
+		"<p align=\"center\"><img src=\"db/onlineactive/l00001/001.jpg\" width=\"100%\" height=\"auto\"></p>" +
 		"<p>Fusce eros lectus, accumsan eget mi vel, iaculis tincidunt felis. Nulla tincidunt pharetra sagittis. Fusce in felis eros. Nulla sit amet aliquam lorem, et gravida ipsum. Mauris consectetur nisl non sollicitudin tristique. Praesent vitae metus ac quam rhoncus mattis vel et nisi. Aenean aliquet, felis quis dignissim iaculis, lectus quam tincidunt ligula, et venenatis turpis risus sed lorem. Morbi eu metus elit. Ut vel diam dolor."
+
+	this.Data["title"] = title
+	this.Data["date"] = date
+	this.Data["category"] = form + " | " + category
+	this.Data["content"] = "<p><strong>" + "嘉宾: " + host + "</strong></p>" +
+		"<p><strong>" + "主持: " + guest + "</strong></p>" +
+		"<p><i>" + summary + "</i></p>" +
+		"<p>" + content + "</p></div>"
 
 	this.TplNames = "details.html"
 }
